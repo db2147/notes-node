27e2fedf09b2b035eba8b3ba9ed39a5c6ae9318a
KrisB <david.bierma@gmail.com>
2018-08-28 16:56:46 -0500

    comment app.js file

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-console.log('Starting app.');
+console.log('Starting app.'); // after we type node app.js to start the application it logs 'Starting app.' in the console
 
 const fs = require('fs');
 const os = require('os'); // requiring os which is a part of node (does not need to be installed)
 
-fs.appendFileSync('greetings.txt', 'Hello world!');
+var user = os.userInfo(); // if we console log this and run the application it will display user info in the command line
+
+fs.appendFileSync('greetings.txt', `Hello ${user.username}!`); // when the program is run, node will create the 'greetings.txt' file 
+                                                               // and add user text info to the new file
