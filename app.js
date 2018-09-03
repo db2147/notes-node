f2ee13e40e7c7c1ec74b092f8daf15ba53b62bde
KrisB <david.bierma@gmail.com>
2018-09-03 17:58:37 -0500

    exporting 4 functions into app.js that add, list, read and remove notes

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 console.log('Starting app.js'); // after we type node app.js to start the application it logs 'Starting app.' in the console
 
-const fs = require('fs');
-const _  = require('lodash');
-const yargs = require('yargs');
+const fs = require('fs');       // filesystem module which is built into node
+const _  = require('lodash');   // npm package we install
+const yargs = require('yargs'); // helps with parsing command line arguments
 
 const notes = require('./notes.js'); // creating a const which requires a filepath from notes.js
 
-const argv = yargs.argv;
-var command = process.argv[2];
+const argv = yargs.argv;        // creating a const and setting it equal to yargs.argv (like the yargs is parsing the argv)
+var command = argv._[0];        // variable set equal to argv which equals yargs.argv; connecting to lodash npm package and passing in the first argument
 console.log('Command: ', command);
-console.log('Process', process.argv);
 console.log('Yargs', argv);
 
 
-if (command === 'add') {
-    notes.addNote(argv.title, argv.body);
-} else if (command === 'list') {
-    console.log('Listing all notes');
-} else if (command === 'read') {
-    console.log('Reading note');
-} else if (command === 'remove') {
-    console.log('Removing note');
+if (command === 'add') {                // add command passes in the title and body of the note when run
+    notes.addNote(argv.title, argv.body);   
+} else if (command === 'list') {        // lists all notes with nothing passed in as an argument
+    notes.getAll();
+} else if (command === 'read') {        // reads a specific note and prints the title in the console
+    notes.getNote(argv.title);
+} else if (command === 'remove') { // passing in the variable argv as an argument in removeNote(argv holds yargs.argv)
+    notes.removeNote(argv.title);
 } else {
     console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,9 +1,26 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => { 
-    console.log('addNote');       // this console.log will let us know that the addNote function was called
-    return 'New note';
-}
+var addNote = (title, body) => { // function addNote adds a note; passing in title and body as the arguments
+    console.log('Adding note', title, body); // console logging will happen when app.js is run and will print to the screen the text and title/body
+};
+
+var getAll = () => { // function will get all notes; no arguments passed into it; will print text when app.js is run
+    console.log('Getting all notes');
+};
+
+var getNote = (title) => { // function gets a single note; passed in the title as an argument
+    console.log('Getting note', title); // when app.js is run it will print the text and the title to the screen
+};
+
+var removeNote = (title) => { // function that passes in the title of the note I want to remove
+    console.log('Removing note', title); // when app.js is run the function will print the text and the title to be removed to the screen
+};
 
 
+module.exports = { // exporting our key/value pairs which in es6 can show up as just the first part if they are the same
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+};
 
