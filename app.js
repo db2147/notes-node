955d1c58e7c8295cf3b1d9e0d2ac7780364911ff
KrisB <david.bierma@gmail.com>
2018-09-03 09:53:17 -0500

    install lodash npm package and save to package.json

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ console.log('Starting app.js'); // after we type node app.js to start the applic
 
 const fs = require('fs');
 const os = require('os'); // requiring os which is a part of node (does not need to be installed)
-const notes = require('./notes.js'); // creating a filepath to notes.js
+const notes = require('./notes.js'); // creating a const which requires a filepath from notes.js
 
 
-var res = notes.add(3, 5);
-console.log(res);
+var res = notes.add(3, 5); // the idea is we are importing a function from notes.js that adds two numbers (arguments)
+console.log('Result:', res);
 
 // var user = os.userInfo();  
 
